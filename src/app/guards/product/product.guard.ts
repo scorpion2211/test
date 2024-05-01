621a2b20a393f2edc7ca410cb4b58f81ed944646
Ignacio Arias <iariasdev1@gmail.com>
2024-05-01 14:00:30 -0300

    fix lint

--- src/app/guards/product/product.guard.ts
+++ src/app/guards/product/product.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { ProductsService } from 'src/app/services/products/products.service';
 import { IDataRecord } from 'src/app/shared/utils/records.interface';
